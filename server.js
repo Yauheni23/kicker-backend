ee245f4221eec3b4b22a795187d7e476657ca002
Eugene <e.yarmolich@itransition.com>
2020-02-24 14:58:22 +0300

    add creating database script

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ app.use('/uploads', express.static('uploads'));
 require('./app/router')(app);
 
 app.listen(process.env.PORT || 8080, () => {
-    console.log(process.env.DB_HOST)
+    console.log(process.env.DB_HOST);
     console.log('App listening on port 8080!');
 });
-//
-// const db = require('./app/db/db.config.js');
-//
-// db.sequelize.sync({force: true}).then(() => {
-// 	console.log('Drop and Resync with { force: true }');
-// });
