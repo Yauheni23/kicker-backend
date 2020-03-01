9167fce86bfb08a00d3211ee0f66b40784fb2f77
Eugene <e.yarmolich@itransition.com>
2020-03-01 22:14:51 +0300

    refactor code

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ require('./app/router')(app);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(process.env.DB_HOST);
-    console.log('App listening on port 8080!');
+    console.log(`App listening on port ${process.env.PORT || 8080}!`);
 });
